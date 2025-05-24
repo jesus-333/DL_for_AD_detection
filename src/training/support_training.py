4aedaa4d1e56bc537e22a3e63e4befb7f433783c
jesus <albi25.zanca@gmail.com>
2025-05-24 11:00:04 +0200

    Update src support functions (training)
    Add code to create optimizer and update functions to check config

--- src/training/support_training.py
+++ src/training/support_training.py
@@ -1,27 +1,28 @@
 
 """
-Support function used for the training
+Support functions used for the training
 
 @author: Alberto Zancanaro (Jesus)
 @organization: Luxembourg Centre for Systems Biomedicine (LCSB)
 """
 
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 import torch
 
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+def update_log_dict_metrics(metrics_dict, log_dict, label = None):
+    for key, value in metrics_dict.items() :
+        if label is not None :
+            log_dict['{}_{}'.format(key, label)] = value
+        else :
+            log_dict['{}'.format(key)] = value
 
 def check_train_config(config : dict) :
 
     if 'batch_size' not in config :
         raise ValueError('The training configuration must contain the key "batch_size"')
-
-    if 'lr' not in config :
-        raise ValueError('The training configuration must contain the key "lr"')
-
-    if config['lr'] <= 0 : 
-        raise ValueError(f"The learning rate must be greater than 0. Current value: {config['lr']}")
 
     if 'epochs' not in config :
         raise ValueError('The training configuration must contain the key "epochs"')
@@ -44,7 +45,7 @@ def check_train_config(config : dict) :
 
     if 'device' not in config :
         print('Warning: the training configuration does not contain the key "device". "cpu" will be used as default value')
-        config['device'] = "cpu" 
+        config['device'] = "cpu"
 
     if 'path_to_save_model' not in config :
         print('Warning: the training configuration does not contain the key "path_to_save_model". "model_weights" will be used as default value')
@@ -62,7 +63,7 @@ def check_train_config(config : dict) :
 
     if 'measure_metrics_during_training' not in config :
         print('Warning: the training configuration does not contain the key "measure_metrics_during_training". True will be used as default value')
-        config['measure_metrics_during_training'] = True 
+        config['measure_metrics_during_training'] = True
 
     if 'print_var' not in config :
         print('Warning: the training configuration does not contain the key "print_var". True will be used as default value')
@@ -95,6 +96,175 @@ def check_train_config(config : dict) :
             print('Warning: the training configuration does not contain the key "debug". False will be used as default value')
             print('This key is not used in the training functions. It is only useful if you want to quickly filter the training run in wandb')
             config['debug'] = False
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# optimizer config config
+
+def get_optimizer(optimizer_config : dict, model : torch.nn.Module) :
+    """
+    Create and return the optimizer based on the configuration provided.
+
+    Parameters
+    ----------
+    optimizer_config : dict
+        The configuration of the optimizer. The configuration must contain the key 'name' with the name of the optimizer to use. Then name must be the same of hte class in torch.optim.
+        Currently implemented: Adam, AdamW, LBFGS, SGD.
+        The configuration must contain the parameters required by the optimizer. For example, if the name is 'AdamW', the configuration must contain the key 'lr', 'betas', 'eps', 'weight_decay', 'amsgrad', 'maximize'. Parameters like 'capturable', 'differentiable', 'foreach' are not implemented and they are set to their default value.
+        If a parameter is not present in the configuration, the function will throw an error. You can check the parameters through the function check_optimizer_config. The functions will check if the parameters present in the configuration are valid and will set the missing ones to their default value.
+    model : torch.nn.Module
+        The model to use with the optimizer. The model must be created before calling this function. The model must be the same as the one used in the training function.
+    """
+
+    if optimizer_config['name'] == 'Adam' :
+        optimizer = torch.optim.Adam(model.parameters(), lr = optimizer_config['lr'], betas = optimizer_config['betas'], eps = optimizer_config['eps'], weight_decay = optimizer_config['weight_decay'], decoupled_weight_decay = optimizer_config['decoupled_weight_decay'], amsgrad = optimizer_config['amsgrad'], maximize = optimizer_config['maximize'])
+    elif optimizer_config['name'] == 'AdamW' :
+        optimizer = torch.optim.AdamW(model.parameters(), lr = optimizer_config['lr'], betas = optimizer_config['betas'], eps = optimizer_config['eps'], weight_decay = optimizer_config['weight_decay'], amsgrad = optimizer_config['amsgrad'], maximize = optimizer_config['maximize'])
+    elif optimizer_config['name'] == 'LBFGS' :
+        optimizer = torch.optim.LBFGS(model.parameters(), lr = optimizer_config['lr'], max_iter = optimizer_config['max_iter'], max_eval = optimizer_config['max_eval'], tolerance_grad = optimizer_config['tolerance_grad'], tolerance_change = optimizer_config['tolerance_change'], history_size = optimizer_config['history_size'], line_search_fn = optimizer_config['line_search_fn'])
+    elif optimizer_config['name'] == 'SGD' :
+        optimizer = torch.optim.SGD(model.parameters(), lr = optimizer_config['lr'], momentum = optimizer_config['momentum'], dampening = optimizer_config['dampening'], weight_decay = optimizer_config['weight_decay'], nesterov = optimizer_config['nesterov'], maximize = optimizer_config['maximize'])
+    else :
+        raise ValueError(f'The optimizer name is not valid. Curretly implemented: Adam, AdamW, LBFGS, SGD. Value given: {optimizer_config["name"]}')
+
+    return optimizer
+
+def check_optimizer_config(optimizer_config : dict) :
+    """
+    Check the parameters of the optimizer configuration and set the default values for the missing ones.
+    """
+
+    if 'lr' not in optimizer_config :
+        raise ValueError('The optimizer configuration must contain the key "lr"')
+
+    if optimizer_config['lr'] <= 0 :
+        raise ValueError(f"The learning rate must be greater than 0. Current value: {optimizer_config['lr']}")
+
+    if optimizer_config['name'] == 'Adam' :
+        if 'betas' not in optimizer_config :
+            print('Warning: the optimizer configuration does not contain the key "betas". (0.9, 0.999) will be used as default value')
+            optimizer_config['betas'] = (0.9, 0.999)
+        if 'eps' not in optimizer_config :
+            print('Warning: the optimizer configuration does not contain the key "eps". 1e-8 will be used as default value')
+            optimizer_config['eps'] = 1e-8
+        if 'weight_decay' not in optimizer_config :
+            print('Warning: the optimizer configuration does not contain the key "weight_decay". 0 will be used as default value')
+            optimizer_config['weight_decay'] = 0
+        if 'decoupled_weight_decay' not in optimizer_config :
+            print('Warning: the optimizer configuration does not contain the key "decoupled_weight_decay". False will be used as default value')
+            optimizer_config['decoupled_weight_decay'] = False
+        if 'amsgrad' not in optimizer_config :
+            print('Warning: the optimizer configuration does not contain the key "amsgrad". False will be used as default value')
+            optimizer_config['amsgrad'] = False
+        if 'maximize' not in optimizer_config :
+            print('Warning: the optimizer configuration does not contain the key "maximize". False will be used as default value')
+            optimizer_config['maximize'] = False
+    elif optimizer_config['name'] == 'AdamW' :
+        if 'betas' not in optimizer_config :
+            print('Warning: the optimizer configuration does not contain the key "betas". (0.9, 0.999) will be used as default value')
+            optimizer_config['betas'] = (0.9, 0.999)
+        if 'eps' not in optimizer_config :
+            print('Warning: the optimizer configuration does not contain the key "eps". 1e-8 will be used as default value')
+            optimizer_config['eps'] = 1e-8
+        if 'weight_decay' not in optimizer_config :
+            print('Warning: the optimizer configuration does not contain the key "weight_decay". 1e-2 will be used as default value')
+            optimizer_config['weight_decay'] = 1e-2
+        if 'amsgrad' not in optimizer_config :
+            print('Warning: the optimizer configuration does not contain the key "amsgrad". False will be used as default value')
+            optimizer_config['amsgrad'] = False
+        if 'maximize' not in optimizer_config :
+            print('Warning: the optimizer configuration does not contain the key "maximize". False will be used as default value')
+            optimizer_config['maximize'] = False
+    elif optimizer_config['name'] == 'LBFGS' :
+        if 'max_iter' not in optimizer_config :
+            print('Warning: the optimizer configuration does not contain the key "max_iter". 20 will be used as default value')
+            optimizer_config['max_iter'] = 20
+        if 'max_eval' not in optimizer_config :
+            print('Warning: the optimizer configuration does not contain the key "max_eval". max_eval * 1.25 will be used as default value')
+            optimizer_config['max_eval'] = optimizer_config['max_iter'] * 1.25
+        if 'tolerance_grad' not in optimizer_config :
+            print('Warning: the optimizer configuration does not contain the key "tolerance_grad". 1e-7 will be used as default value')
+            optimizer_config['tolerance_grad'] = 1e-7
+        if 'tolerance_change' not in optimizer_config :
+            print('Warning: the optimizer configuration does not contain the key "tolerance_change". 1e-9 will be used as default value')
+            optimizer_config['tolerance_change'] = 1e-9
+        if 'history_size' not in optimizer_config :
+            print('Warning: the optimizer configuration does not contain the key "history_size". 100 will be used as default value')
+            optimizer_config['history_size'] = 100
+        if 'line_search_fn' not in optimizer_config :
+            print('Warning: the optimizer configuration does not contain the key "line_search_fn". None will be used as default value')
+            optimizer_config['line_search_fn'] = None
+    elif optimizer_config['name'] == 'SGD' :
+        if 'momentum' not in optimizer_config :
+            print('Warning: the optimizer configuration does not contain the key "momentum". 0 will be used as default value')
+            optimizer_config['momentum'] = 0
+        if 'dampening' not in optimizer_config :
+            print('Warning: the optimizer configuration does not contain the key "dampening". 0 will be used as default value')
+            optimizer_config['dampening'] = 0
+        if 'weight_decay' not in optimizer_config :
+            print('Warning: the optimizer configuration does not contain the key "weight_decay". 0 will be used as default value')
+            optimizer_config['weight_decay'] = 0
+        if 'nesterov' not in optimizer_config :
+            print('Warning: the optimizer configuration does not contain the key "nesterov". False will be used as default value')
+            optimizer_config['nesterov'] = False
+        if 'maximize' not in optimizer_config :
+            print('Warning: the optimizer configuration does not contain the key "maximize". False will be used as default value')
+            optimizer_config['maximize'] = False
+    else :
+        raise ValueError(f'The optimizer name is not valid. Curretly implemented: Adam, AdamW, LBFGS, SGD. Value given: {optimizer_config["name"]}')
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# lr scheduler config
+
+def get_lr_scheduler(lr_scheduler_config : dict, optimizer)  :
+    """
+    Return the learning rate scheduler based on the configuration provided.
+
+    Parameters
+    ----------
+    lr_scheduler_config : dict
+        The configuration of the learning rate scheduler. The configuration must contain the key 'name' with the name of the scheduler to use. Then name must be the same of hte class in torch.optim.lr_scheduler.
+        Currently implemented: ExponentialLR, CosineAnnealingLR, CosineAnnealingWarmRestarts, StepLR, CyclicLR, ChainedScheduler (the chained scheduler must be composed of other schedulers already implemented in this function).
+        The configuration must contain the parameters required by the scheduler. For example, if the name is 'ExponentialLR', the configuration must contain the key 'gamma' with the value of gamma to use.
+        If the name is 'ChainedScheduler', the configuration must contain the key 'list_config_schedulers'. The value of this key must be a dictionary with the name of the scheduler as key and the configuration as value. The configuration of the single scheduler must be the same as the one used in this function.
+        Example of config for the chained scheduler :
+            lr_scheduler_config = dict(
+                name = 'ChainedScheduler',
+                list_config_schedulers = dict(
+                    config_1 = dict(
+                        name = 'CosineAnnealingLR',
+                        T_max = 10,
+                        eta_min = 3 * 1e-5,
+                    ),
+                    config_2 = dict(
+                        name = 'ExponentialLR',
+                        gamma = 0.954,
+                    )
+                )
+            )
+    optimizer : torch.optim.Optimizer
+        The optimizer to use with the learning rate scheduler. The optimizer must be created before calling this function. The optimizer must be the same as the one used in the training function.
+    """
+    if lr_scheduler_config['name'] == 'ExponentialLR' :
+        lr_scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma = lr_scheduler_config['gamma'])
+    elif lr_scheduler_config['name'] == 'CosineAnnealingLR' :
+        lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max = lr_scheduler_config['T_max'], eta_min = lr_scheduler_config['eta_min'])
+    elif lr_scheduler_config['name'] == 'CosineAnnealingWarmRestarts' :
+        lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingWarmRestarts(optimizer, T_0 = lr_scheduler_config['T_0'], T_mult = lr_scheduler_config['T_mult'], eta_min = lr_scheduler_config['eta_min'])
+    elif lr_scheduler_config['name'] == 'StepLR' :
+        lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size = lr_scheduler_config['step_size'], gamma = lr_scheduler_config['gamma'])
+    elif lr_scheduler_config['name'] == 'CyclicLR' :
+        lr_scheduler = torch.optim.lr_scheduler.CyclicLR(optimizer, base_lr = lr_scheduler_config['base_lr'], max_lr = lr_scheduler_config['max_lr'], step_size_up = lr_scheduler_config['step_size_up'], step_size_down = lr_scheduler_config['step_size_down'], mode = lr_scheduler_config['mode'], gamma = lr_scheduler_config['gamma'])
+    elif lr_scheduler_config['name'] == 'ChainedScheduler' :
+        schedulers_list = []
+        for name_config in lr_scheduler_config['list_config_schedulers'] :
+            config_single_scheduler = lr_scheduler_config['list_config_schedulers'][name_config]
+            tmp_scheduler = get_lr_scheduler(config_single_scheduler, optimizer)
+            schedulers_list.append(tmp_scheduler)
+        lr_scheduler = torch.optim.lr_scheduler.ChainedScheduler(schedulers_list, optimizer)
+    else :
+        raise ValueError(f'The learning rate scheduler name is not valid. Curretly implemented: ExponentialLR, CosineAnnealingLR, CosineAnnealingWarmRestarts, StepLR, CyclicLR. Value given: {lr_scheduler_config["name"]}')
+
+    return lr_scheduler
 
 def check_lr_scheduler_config(lr_scheduler_config : dict) :
     if 'name' not in lr_scheduler_config :
@@ -141,41 +311,9 @@ def check_lr_scheduler_config(lr_scheduler_config : dict) :
             lr_scheduler_config['gamma'] = 1
     elif lr_scheduler_config['name'] == 'ChainedScheduler' :
         if 'list_config_schedulers' not in lr_scheduler_config :
-            raise ValueError('The learning rate scheduler configuration must contain the key "list_config_schedulers" if the name is ChainedScheduler. See chained_lr_scheduler_example.toml in /scripts/training/config/ for an example.') 
+            raise ValueError('The learning rate scheduler configuration must contain the key "list_config_schedulers" if the name is ChainedScheduler. See chained_lr_scheduler_example.toml in /scripts/training/config/ for an example.')
         for name_config in lr_scheduler_config['list_config_schedulers'] :
             check_lr_scheduler_config(lr_scheduler_config['list_config_schedulers'][name_config])
     else :
         raise ValueError(f'The name of the lr scheduler is not valid. Possible values: ExponentialLR, CosineAnnealingLR, StepLR, ChainedScheduler. Current value: {lr_scheduler_config["name"]}')
-
-def update_log_dict_metrics(metrics_dict, log_dict, label = None):
-    for key, value in metrics_dict.items() :
-        if label is not None :
-            log_dict['{}_{}'.format(key, label)] = value
-        else :
-            log_dict['{}'.format(key)] = value
-
-def get_lr_scheduler(lr_scheduler_config : dict, optimizer)  :
-    if lr_scheduler_config['name'] == 'ExponentialLR' :
-        lr_scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma = lr_scheduler_config['gamma'])
-    elif lr_scheduler_config['name'] == 'CosineAnnealingLR' :
-        lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max = lr_scheduler_config['T_max'], eta_min = lr_scheduler_config['eta_min'])
-    elif lr_scheduler_config['name'] == 'CosineAnnealingWarmRestarts' :
-        lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingWarmRestarts(optimizer, T_0 = lr_scheduler_config['T_0'], T_mult = lr_scheduler_config['T_mult'], eta_min = lr_scheduler_config['eta_min'])
-    elif lr_scheduler_config['name'] == 'StepLR' :
-        lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size = lr_scheduler_config['step_size'], gamma = lr_scheduler_config['gamma'])
-    elif lr_scheduler_config['name'] == 'CyclicLR' :
-        lr_scheduler = torch.optim.lr_scheduler.CyclicLR(optimizer, base_lr = lr_scheduler_config['base_lr'], max_lr = lr_scheduler_config['max_lr'], step_size_up = lr_scheduler_config['step_size_up'], step_size_down = lr_scheduler_config['step_size_down'], mode = lr_scheduler_config['mode'], gamma = lr_scheduler_config['gamma'])
-    elif lr_scheduler_config['name'] == 'ChainedScheduler' :
-        schedulers_list = []
-        for name_config in lr_scheduler_config['list_config_schedulers'] :
-            config_single_scheduler = lr_scheduler_config['list_config_schedulers'][name_config]
-            tmp_scheduler = get_lr_scheduler(config_single_scheduler, optimizer)
-            schedulers_list.append(tmp_scheduler)
-        lr_scheduler = torch.optim.lr_scheduler.ChainedScheduler(schedulers_list, optimizer)
-    else :
-        raise ValueError(f'The learning rate scheduler name is not valid. Curretly implemented: ExponentialLR, CosineAnnealingLR, CosineAnnealingWarmRestarts, StepLR, CyclicLR. Value given: {lr_scheduler_config["name"]}')
-
-    return lr_scheduler
-
-
 
