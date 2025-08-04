Bug correction (float overflow)

src/dataset/support_dataset.py
@@ -83,8 +83,8 @@ def get_idx_to_split_data_V2(n_elements : int, percentage_split_list : list, see
     """
     
     # Check input parameter
-    # Note that check for 0.9999999999999999 and 0.9999999999999998 is due to the float precision
-    possible_sum = [1, 0.9999999999999999, 0.9999999999999998]
+    # Note that check for 0.9999999999999999 and 0.9999999999999998, 1.0000000000000001, 1.0000000000000002 is due to the float precision
+    possible_sum = [1, 0.9999999999999999, 0.9999999999999998, 1.0000000000000001, 1.0000000000000002]
     if n_elements <= 1 : raise ValueError("n_elements must be greater than 1. Current value is {}".format(n_elements))
     if np.sum(percentage_split_list) not in possible_sum : raise ValueError("The sum of the elements in percentage_split_list must be equal to 1. Current sum is {}".format(np.sum(percentage_split_list)))
 