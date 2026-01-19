f52c10fa508f2ae216d3e6fa052bc268823887a1
jesus <albi25.zanca@gmail.com>
2026-01-19 16:36:41 +0100

    Minor update new script to divide data for FL training

--- scripts_python/data_manipulation/create_idx_files_for_federated_simulations.py
+++ scripts_python/data_manipulation/create_idx_files_for_federated_simulations.py
@@ -16,7 +16,7 @@ The folder will contain the following files:
 - ...
 - train_idx_client_{n_client - 1}.npy : indices of the training data for last client
 - val_idx.npy : indices of the validation data
-- train_idx.npy : indices of the training data (all clients) (basically the concatenation of all train_idx_client_{i}.npy files)
+- train_idx_all.npy : indices of the training data (all clients) (basically the concatenation of all train_idx_client_{i}.npy files)
 
 E.g. if n_client is 3 and percentage_data_used_for_training is 0.8, the folder will contain the following files:
 - path_to_save/
@@ -24,41 +24,43 @@ E.g. if n_client is 3 and percentage_data_used_for_training is 0.8, the folder w
     - train_idx_client_1.npy    (data for client 1, roughly 26.67% of the data)
     - train_idx_client_2.npy    (data for client 2, roughly 26.67% of the data)
     - val_idx.npy               (20% of the data)
-    - train_idx.npy             (80% of the data)
+    - train_idx_all.npy         (80% of the data)
 
-If use_cross_validation is set to True, the training data will be split in n_folds folds for cross validation. In this case, percentage_data_used_for_training will be rounded to the closest standard value (0.5, 0.75, 0.8, 0.9, 0.95, 0.98) if it is not one of them.
-The number of folds (n_folds) will be computed as n_folds = 1 / (1 - percentage_data_used_for_training).
+If use_cross_fold_validation is set to True, the training data will be split in n_folds folds for cross fold validation. In this case, there are two possibilities :
+1) You provide the n_folds argument directly.
+2) You do not provide the n_folds argument. In this case, the n_folds will be computed from percentage_data_used_for_training. The number of folds (n_folds) will be computed as n_folds = 1 / (1 - percentage_data_used_for_training).
+Note that for option 1, the provided percentage_data_used_for_training will be ignored and the actual percentage of data used for training will be computed as percentage_data_used_for_training = (n_folds - 1) / n_folds.
+Note that for option 2 instead, the percentage_data_used_for_training will be rounded to the closest standard value (0.5, 0.75, 0.8, 0.9, 0.95, 0.98) to ensure that n_folds is an integer.
 E.g. if percentage_data_used_for_training is 0.8, the number of folds will be 5 (1 / (1 - 0.8) = 1 / 0.2 = 5).
-In case of cross validation, the data will be saved in subfolders named fold_0, fold_1, ..., fold_{n_folds - 1}.
+In case of cross fold validation, the data will be saved in subfolders named fold_0, fold_1, ..., fold_{n_folds - 1}.
 Inside each folder, the files will be named as described before.
-Since the n_of_folds is computed from percentage_data_used_for_training, only some specific values are allowed for the arguments (i.e. values that give an integer number of folds from the formula n_folds = 1 / (1 - percentage_data_used_for_training)).
 
-E.g. if n_client is 3, percentage_data_used_for_training is 0.75 and use_cross_validation is True, the folder will contain the following files:
+E.g. if n_client is 3, percentage_data_used_for_training is 0.75 and use_cross_fold_validation is True and you have not provide n_folds as argument, the final output of this script will be the following structure:
 - path_to_save/
     - fold_0/
         - train_idx_client_0.npy    (data for client 0, roughly 26.67% of the data)
         - train_idx_client_1.npy    (data for client 1, roughly 26.67% of the data)
         - train_idx_client_2.npy    (data for client 2, roughly 26.67% of the data)
         - val_idx.npy               (20% of the data)
-        - train_idx.npy             (80% of the data)
+        - train_idx_all.npy         (80% of the data)
     - fold_1/
         - train_idx_client_0.npy    (data for client 0, roughly 26.67% of the data)
         - train_idx_client_1.npy    (data for client 1, roughly 26.67% of the data)
         - train_idx_client_2.npy    (data for client 2, roughly 26.67% of the data)
         - val_idx.npy               (20% of the data)
-        - train_idx.npy             (80% of the data)
+        - train_idx_all.npy         (80% of the data)
     - fold_2/
         - train_idx_client_0.npy    (data for client 0, roughly 26.67% of the data)
         - train_idx_client_1.npy    (data for client 1, roughly 26.67% of the data)
         - train_idx_client_2.npy    (data for client 2, roughly 26.67% of the data)
         - val_idx.npy               (20% of the data)
-        - train_idx.npy             (80% of the data)
+        - train_idx_all.npy         (80% of the data)
     - fold_3/
         - train_idx_client_0.npy    (data for client 0, roughly 26.67% of the data)
         - train_idx_client_1.npy    (data for client 1, roughly 26.67% of the data)
         - train_idx_client_2.npy    (data for client 2, roughly 26.67% of the data)
         - val_idx.npy               (20% of the data)
-        - train_idx.npy             (80% of the data)
+        - train_idx_all.npy         (80% of the data)
 
 The arguments can be passed through the command line or set inside the script. If used, the argument from the command line will overwrite the ones set inside the script.
 Note that addl is the name of the package built through hatch from the file in the src folder.
@@ -79,10 +81,14 @@ parser.add_argument('--path_to_save'                     , type = str  , default
 parser.add_argument('--percentage_data_used_for_training', type = float, default = None, help = 'Percentage of data to use for training (the rest will be used for validation). If not specified it will use the value defined in this script.')
 parser.add_argument('--n_client'                         , type = int  , default = None, help = 'Number of clients to simulate. If not specified it will use the value defined in this script.')
 parser.add_argument('--seed'                             , type = int  , default = None, help = 'Random seed for reproducibility. It must be a positive integer. If not specified it will use the value defined in this script.')
+parser.add_argument('--n_folds'                          , type = int  , default = None, help = 'Number of folds for cross fold validation. If not specified it will use the value defined in this script.')
 
 # Boolean arguments
-parser.add_argument('--use_cross_validation'             , default = None, action = 'store_true', help = 'If passed the script will use cross validation. See the script header for more details.')
+parser.add_argument('--use_cross_fold_validation'        , default = None, action = 'store_true', help = 'If passed the script will use cross fold validation. See the script header for more details.')
 parser.add_argument('--keep_labels_proportion'           , default = None, action = 'store_true', help = 'If passed, the data will be split between clients trying to keep the same label proportion in each client as in the original dataset.')
+
+# Negate boolean arguments
+parser.add_argument('--no-use_cross_fold_validation'     , action = 'store_false', dest = 'use_cross_fold_validation', help = 'If passed as an argument, no cross-fold validation will be applied')
 
 args = parser.parse_args()
 
@@ -95,7 +101,6 @@ import pandas as pd
 import torch
 
 from addl.federated import support_federated_generic
-from addl.dataset import support_dataset
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Arguments
@@ -104,11 +109,12 @@ path_data = "data/ADNI_axial_middle_slice/"
 name_tensor_file = "dataset_tensor___176_resize.pt"
 path_to_save = "data/ADNI_axial_middle_slice/FL_idx/"
 
-percentage_data_used_for_training = 0.85
+percentage_data_used_for_training = 0.8
 n_client = 5
-seed = 42
+seed = -1
+n_folds = None # If None, it will be computed from percentage_data_used_for_training
 
-use_cross_validation = True
+use_cross_fold_validation = True
 keep_labels_proportion = True
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -122,7 +128,8 @@ if args.path_to_save is not None                      : path_to_save = args.path
 if args.percentage_data_used_for_training is not None : percentage_data_used_for_training = args.percentage_data_used_for_training
 if args.n_client is not None                          : n_client = args.n_client
 if args.seed is not None                              : seed = args.seed
-if args.use_cross_validation is not None              : use_cross_validation = args.use_cross_validation
+if args.n_folds is not None                           : n_folds = args.n_folds
+if args.use_cross_fold_validation is not None         : use_cross_fold_validation = args.use_cross_fold_validation
 if args.keep_labels_proportion is not None            : keep_labels_proportion = args.keep_labels_proportion
 
 # Check arguments validity
@@ -134,8 +141,8 @@ if not (0.0 < percentage_data_used_for_training < 1.0) :
     raise ValueError(f"percentage_data_used_for_training must be between 0 and 1. Current value: {percentage_data_used_for_training}")
 
 possible_values_percentage_data_used_for_training = [0.5, 0.75, 0.8, 0.9, 0.95, 0.98]
-if percentage_data_used_for_training not in possible_values_percentage_data_used_for_training and use_cross_validation :
-    print(f"Warning: percentage_data_used_for_training is set to {percentage_data_used_for_training}. If you enable cross validation, it is recommended to use one of the standard values: {possible_values_percentage_data_used_for_training}")
+if percentage_data_used_for_training not in possible_values_percentage_data_used_for_training and use_cross_fold_validation :
+    print(f"Warning: percentage_data_used_for_training is set to {percentage_data_used_for_training}. If you enable cross fold validation, it is recommended to use one of the standard values: {possible_values_percentage_data_used_for_training}")
     print("percentage_data_used_for_training will be rounded to the closest standard value.")
     percentage_data_used_for_training = min(possible_values_percentage_data_used_for_training, key = lambda x : abs(x - percentage_data_used_for_training))
     print(f"New value for percentage_data_used_for_training: {percentage_data_used_for_training}")
@@ -148,7 +155,7 @@ os.makedirs(path_to_save, exist_ok = True)
 def split_and_save_indices(idx_train, idx_val, labels_train, path_to_save : str, print_info : bool = True) :
     """
     Split the training data between clients and save the indices files (both training and validation).
-    The function was created to avoid code duplication when using cross validation.
+    The function was created to avoid code duplication when using cross fold validation.
     """
 
     # Get labels for training data
@@ -174,7 +181,7 @@ def split_and_save_indices(idx_train, idx_val, labels_train, path_to_save : str,
 
     # Save the indices files
     np.save(f'{path_to_save}val_idx.npy', idx_val)
-    np.save(f'{path_to_save}train_idx.npy', idx_train)
+    np.save(f'{path_to_save}train_idx_all.npy', idx_train)
     for i in range(n_client) : np.save(f'{path_to_save}train_idx_client_{i}.npy', idx_clients_train[i])
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -186,10 +193,10 @@ print(f"Total number of samples in the dataset : {len(all_idx)}\n")
 dataset_info = pd.read_csv(f'{path_data}dataset_info.csv')
 all_labels_int = dataset_info['labels_int'].to_numpy()
 
-if use_cross_validation :
-    # Compute the number of folds.
+if use_cross_fold_validation :
+    # (OPTIONAL) Compute the number of folds.
     # The int function is used to avoid problems with floating point precision (e.g. 1 / (1 - 0.98) = 49.99999999999996 on my machine)
-    n_folds = int(1 / (1 - percentage_data_used_for_training))
+    if n_folds is None : n_folds = int(1 / (1 - percentage_data_used_for_training))
 
     # Split the data uniformly between folds
     # Note that this function was originally designed to split data (uniformly) between clients, but here we can exploit it to split data uniformly between folds
