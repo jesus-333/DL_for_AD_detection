eff28b197cb45f04fde8be1e87cf0446eccbf0dc
jesus <albi25.zanca@gmail.com>
2026-05-27 17:02:51 +0200

    Add vav training script (sh)

---
+++ scripts_sh_V2/vav_cent.sh
@@ -0,0 +1,99 @@
+#!/bin/sh
+
+#SBATCH --job-name="train_vav_CENTRALIZED_V2"
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=4
+#SBATCH --gpus-per-task=1
+#SBATCH --partition=hopper
+#SBATCH --qos=iris-hopper
+#SBATCH --mem=13G
+#SBATCH --time=0-01:00:00
+#SBATCH --mail-user=alberto.zancanaro@uni.lu
+#SBATCH --mail-type=end,fail 
+#SBATCH --output=./scripts_sh_V2/output/vav/std_%x_%j.txt
+#SBATCH --error=./scripts_sh_V2/output/vav/err_%x_%j.txt
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+# Load python environment
+
+echo "---------------------------------------------------"
+echo $CONDA_DEFAULT_ENV
+echo "---------------------------------------------------"
+conda init
+conda activate jesus-hpc
+echo "+++++++++++++++++++++++++++++++++++++++++++++++++++"
+echo $CONDA_DEFAULT_ENV
+echo "+++++++++++++++++++++++++++++++++++++++++++++++++++"
+
+hatchling build
+pip install .
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+# Get arguments passed to the script.
+
+path_src=$1
+path_dataset_config=$2
+path_model_config=$3
+path_training_config=$4
+path_optimizer_config=$5
+path_lr_scheduler_config=$6
+path_to_idx_files=$7
+seed=$8
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+# Print arguments (for debugging purposes).
+
+echo "path_src: ${path_src}"
+echo "path_dataset_config: ${path_dataset_config}"
+echo "path_model_config: ${path_model_config}"
+echo "path_training_config: ${path_training_config}"
+echo "path_optimizer_config: ${path_optimizer_config}"
+echo "path_lr_scheduler_config: ${path_lr_scheduler_config}"
+echo "path_to_idx_file: ${path_to_idx_files}"
+echo "seed: ${seed}"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+# Prepare data. 
+# I can use the same script to divide the data used for FL learning because the script also create an idx fill with all the indices of all training data together
+# And I use the server data as validation set
+
+path_data=$(toml get -r ${path_dataset_config} path_data)
+name_hdf5_file=$(toml get -r ${path_dataset_config} name_hdf5_file)
+path_hdf5_file="${path_data}/${name_hdf5_file}"
+
+n_samples=$(get-hdf5-dataset-length --path_hdf5_file ${path_hdf5_file})
+percentage_data_used_for_training=$(toml get -r ${path_dataset_config} percentage_train)
+
+python ./scripts_python/data_manipulation/create_idx_files_for_federated_simulations_2.py\
+	--path_data=${path_data}\
+	--n_sammples=${n_samples}\
+	--path_to_save=${path_to_idx_files}\
+	--percentage_data_used_for_training=${percentage_data_used_for_training}\
+	--num_clients=2\
+	--seed=${seed}\
+	--no-use_cross_fold_validation\
+	--keep_samples_proportion\
+
+echo "Data preparation END"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+# Update training config. 
+python ./scripts_python/training/update_training_config.py\
+	--path_training_config="${path_training_config}"\
+	--path_optimizer_config="${path_optimizer_config}"\
+	--path_lr_scheduler_config="${path_lr_scheduler_config}"\
+	--path_to_save_model="model_weights/vav/${seed}_${SLURM_JOB_ID}/"\
+	--seed="${seed}"\
+	--no-fl_training\
+	--no-vgg_training\
+	--no-swin_training\
+	--vav_training\
+
+python ./scripts_python/training/vav_centralized_V2.py\
+	--path_src="${path_src}"\
+	--path_dataset_config="${path_dataset_config}"\
+	--path_model_config="${path_model_config}"\
+	--path_training_config="${path_training_config}"\
+	--path_to_idx_files=${path_to_idx_files}\
